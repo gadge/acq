80b7d75e81153724e0f41bf4a72cb33cd333e375
alkashi <hoyeungw@outlook.com>
2020-03-02 11:34:30 +0800

    changed Acq.port to Acq.tabular

--- obsolete/enum-ret/index.js
+++ obsolete/enum-ret/index.js
@@ -3,6 +3,10 @@ const JSON = 0
 const SAMPLES = 1
 const TABLE = 2
 
+/**
+ *
+ * @type {{TABLE: number, json: number, JSON: number, SAMPLES: number, ANSI: number, samples: number, table: number, ansi: number}}
+ */
 export const ReT = {
   SAMPLES,
   JSON,
