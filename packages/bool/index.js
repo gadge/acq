ae319363e6ab5bb93fecfc3647a06052f7668dca
alkashi <hoyeungw@outlook.com>
2020-03-02 11:35:46 +0800

    changed Acq.port to Acq.tabular

--- packages/bool/index.js
+++ packages/bool/index.js
@@ -1,5 +1,10 @@
 import { STR } from '@typen/enums'
 
+/**
+ *
+ * @param {string|*} v
+ * @returns {boolean}
+ */
 export const bool = v => {
   if (!v?.length) return false
   if (typeof v === STR) return (v = v[0].toLowerCase()) !== 'f' && v !== 'n'
