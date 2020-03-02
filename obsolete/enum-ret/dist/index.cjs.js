f225b167445e0b55be8d20983943f53ee625e0b8
alkashi <hoyeungw@outlook.com>
2020-03-02 11:32:35 +0800

    changed Acq.port to Acq.tabular

---
+++ obsolete/enum-ret/dist/index.cjs.js
@@ -0,0 +1,20 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const ANSI = -1;
+const JSON = 0;
+const SAMPLES = 1;
+const TABLE = 2;
+const ReT = {
+  SAMPLES,
+  JSON,
+  TABLE,
+  ANSI,
+  samples: SAMPLES,
+  json: JSON,
+  table: TABLE,
+  ansi: ANSI
+};
+
+exports.ReT = ReT;
---
+++ obsolete/enum-ret/dist/index.esm.js
@@ -0,0 +1,16 @@
+const ANSI = -1;
+const JSON = 0;
+const SAMPLES = 1;
+const TABLE = 2;
+const ReT = {
+  SAMPLES,
+  JSON,
+  TABLE,
+  ANSI,
+  samples: SAMPLES,
+  json: JSON,
+  table: TABLE,
+  ansi: ANSI
+};
+
+export { ReT };
