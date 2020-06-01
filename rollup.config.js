403047a3e365902c90b911b5b8bd099683fe3dbd
alkashi <hoyeungw@outlook.com>
2020-06-01 18:30:44 +0800

    add url-helper

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,14 @@
-import commonjs    from '@rollup/plugin-commonjs'
-import json        from '@rollup/plugin-json'
-import nodeResolve from '@rollup/plugin-node-resolve'
-import babel       from 'rollup-plugin-babel'
-import fileInfo    from 'rollup-plugin-fileinfo'
+import commonjs       from '@rollup/plugin-commonjs'
+import json           from '@rollup/plugin-json'
+import nodeResolve    from '@rollup/plugin-node-resolve'
+import { decoObject } from '@spare/logger'
+import babel          from 'rollup-plugin-babel'
+import fileInfo       from 'rollup-plugin-fileinfo'
 
 const { name, dependencies, main, module } = require(process.cwd() + '/package.json')
 
 console.log('EXECUTING', name, process.cwd())
-console.log('Dependencies', dependencies)
+console.log('Dependencies', decoObject(dependencies || {}))
 
 const babelPluginOptions = {
   babelrc: false,
