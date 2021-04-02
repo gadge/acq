4b55c5284abbefc4778899ee3d91552f9980047a
hazen <hoyeungw@outlook.com>
2021-04-02 09:43:26 +0800

    update dependencies

--- packages/environ/test/info.test.js
+++ packages/environ/test/info.test.js
@@ -2,8 +2,8 @@ import { deco, logger, logNeL } from 'xbrief'
 import { Environ } from '../index'
 
 'device' |> logger
-Environ.deviceInfo() |> (_ => deco(_, { vu: 2 })) |> logNeL
+Environ.deviceInfo() |> (_ => deco(_, { vert: 2 })) |> logNeL
 'load info' |> logger
-Environ.loadInfo() |> (_ => deco(_, { vu: 1 })) |> logNeL
+Environ.loadInfo() |> (_ => deco(_, { vert: 1 })) |> logNeL
 'ip' |> logger
-Environ.ipInfo() |> (_ => deco(_, { vu: 2 })) |> logNeL
+Environ.ipInfo() |> (_ => deco(_, { vert: 2 })) |> logNeL
