df5d665f4c597565bdeed439f7a1128600870154
Hoyeung <hoyeungw@outlook.com>
2021-11-03 17:34:20 +0800

    minor fix to Chrome

--- packages/chrome/src/Chrome.js
+++ packages/chrome/src/Chrome.js
@@ -1,7 +1,8 @@
-import { Contractor } from '@geia/promise-lobby'
-import { NUM }        from '@typen/enum-data-types'
-import { init }       from '@vect/vector-init'
-import puppeteer      from 'puppeteer'
+import { decoFlat, decoString, says, Xr } from '@spare/logger'
+import { NUM }                            from '@typen/enum-data-types'
+import { init }                           from '@vect/vector-init'
+import puppeteer, { Page }                from 'puppeteer'
+import { Contractor }                     from './Contractor'
 
 /**
  *
@@ -16,20 +17,20 @@ export class Chrome {
     return chrome
   }
 
-  async openBrowser() {
-    const options = {
-      // headless: false,
-      // slowMo: 250,
-    } // slow down by 250ms}
-    return ( this.browser = await puppeteer.launch(options) ), this
+  // const options = {
+  //   headless: false,
+  //   slowMo: 250,// slow down by 250ms
+  // }
+  async openBrowser(options = {}) {
+    this.browser = await puppeteer.launch(options)
+    return this
   }
-  async closeBrowser() { return ( await this.browser.close() ), !this.browser?.isConnected() }
 
-  async openTabs(info) {
-    if (typeof info === NUM) return await Promise.all(init(info, () => this.newTab()))
-    if (Array.isArray(info)) return await Promise.all(info.map((url) => this.newTab(url)))
-    return null
+  async closeBrowser() {
+    await this.browser.close()
+    return !this.browser?.isConnected()
   }
+
 
   async newTab(url) {
     const page = await this.browser.newPage()
@@ -37,18 +38,39 @@ export class Chrome {
     return page
   }
 
-  static async closeTabs(tabs) { return await Promise.all(tabs.map(page => page.close())) }
+  async openTabs(info) {
+    if (typeof info === NUM) return await Promise.all(init(info, () => this.newTab()))
+    if (Array.isArray(info)) return await Promise.all(info.map((url) => this.newTab(url)))
+    return null
+  }
 
-  async openPages({ urls, selector, limit }) {
-    const pageSelector = async function (url) {
+  async closeTabs(tabs) {
+    return await Promise.all(tabs.map(page => page.close()))
+  }
+
+  async evalPages({ urls, selector, limit, log }) {
+    const pageSelector = PageSelectorFactory.build(selector)
+    const pages = await this.openTabs(limit)
+    if (log) Xr().pages(decoFlat(pages.map(pageId))) |> says['chrome'].br('created')
+    const contractor = Contractor.build(pageSelector, pages, pageId) // the internal pageSelector use page as 'this'
+    const samples = await contractor.takeOrders(urls, log)
+    if (log) Xr().pages(decoFlat(pages.map(pageId))) |> says['chrome'].br('closing')
+    await this.closeTabs(pages)
+    return samples
+  }
+}
+
+class PageSelectorFactory {
+  static build(selector) {
+    return async function (url) {
       /** @type {Page} */ const page = this
       await page.goto(url)
       return await selector(page) // input selector use page as arg
     }
-    const pages = await this.openTabs(limit)
-    const contractor = Contractor.build(pageSelector, pages) // the internal pageSelector use page as 'this'
-    const samples = contractor.takeOrders(urls)
-    await Chrome.closeTabs(pages)
-    return samples
   }
+}
+
+export const pageId = page => {
+  const id = page.mainFrame()._id
+  return decoString(id.slice(0, 2) + '-' + id.slice(-4))
 }
